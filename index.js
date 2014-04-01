94c4681ad70501c2791df7fc1b8f96a15c911211
Jinma li <lijinma@126.com>
2014-04-02 00:31:59 +0800

    - add current-time

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ var connect = require('connect');
 
 module.exports = function() {
   var app = connect();
+  app.use(function(req, res, next) {
+    if(req.url == '/current-time') {
+      res.write((new Date()).toISOString());
+      res.end('\n');
+    } else {
+      next();
+    }
+  })
   return app;
 }
